e3de9977a953ea64e40f5cbb9bc766b4618201c1
Sakshi Arora <sakshiarora.arora2002@gmail.com>
2025-04-01 16:17:13 +0530

    updated

--- mealwise-backend/src/main/java/com/mealwise/mealwise_backend/config/DynamoDBConfig.java
+++ mealwise-backend/src/main/java/com/mealwise/mealwise_backend/config/DynamoDBConfig.java
@@ -1,33 +1,19 @@
 package com.mealwise.mealwise_backend.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
 public class DynamoDBConfig {
 
-    @Value("${aws.dynamodb.accessKeyId}")
-    private String accessKey;
-
-   @Value("${aws.dynamodb.secretKey}")
-   private String secretKey;
-
- @Value("${aws.dynamodb.region}")
- private String region;
-
-
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)
-                ))
+                .region(Region.of("ap-south-1")) // Change if needed
+                .credentialsProvider(ProfileCredentialsProvider.create("default")) // uses credentials from ~/.aws/credentials
                 .build();
     }
 }
